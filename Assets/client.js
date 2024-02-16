40c561bca3b900fce27172b238c473fb031c6484
AlishaAroob <aroob.dev@gmail.com>
2024-02-16 12:54:59 +0500

    changes were made

--- Assets/client.js
+++ Assets/client.js
@@ -4,6 +4,18 @@ const form = document.getElementById("send-container");
 const messageinput = document.getElementById("messageinput");
 const messagecontainer = document.querySelector(".container");
 
-const names = prompt("Enter your name to join the chat");
-socket.emit("new-user-joined", names);
-console.log("new-user-joined", names);
+const append = (message, position) => {
+  const messageElement = document.createElement("div");
+  messageElement.innertext = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+  messagecontainer.append(messageElement);
+};
+const name = prompt("Enter your name to join the chat");
+socket.emit("new-user-joined", name);
+
+socket.on =
+  ("user-joined",
+  (name) => {
+    append(`${name} joined the chat`, "right");
+  });
